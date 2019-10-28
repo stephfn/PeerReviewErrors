66c258c3b39f1db57cef96c93887f583b8da8e5a
stephfn <56990020+stephfn@users.noreply.github.com>
2019-10-27 20:16:25 -0500

    edit spacing
    error in indention space

--- peerReviewErrors.py
+++ peerReviewErrors.py
@@ -18,7 +18,7 @@ def displayIntro():
 	print()
 
 def chooseCave():
-    cave = ''
+	cave = ''
 	while cave != '1' and cave != '2':
 		print('Which cave will you go into? (1 or 2)')
 		cave = input()
