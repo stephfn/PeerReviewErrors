cb4e3ce4e6258350c4fc6ae37668958a6700ff3c
stephfn <56990020+stephfn@users.noreply.github.com>
2019-10-27 20:10:04 -0500

    update the way the message is printed

--- peerReviewErrors.py
+++ peerReviewErrors.py
@@ -11,10 +11,10 @@ import random
 import time
 
 def displayIntro():
-	print('''You are in a land full of dragons. In front of you,
-	you see two caves. In one cave, the dragon is friendly
-	and will share his treasure with you. The other dragon
-	is greedy and hungry, and will eat you on sight.''')
+	print('You are in a land full of dragons. In front of you,')
+	print('you see two caves. In one cave, the dragon is friendly')
+	print('and will share his treasure with you. The other dragon')
+	print('is greedy and hungry, and will eat you on sight.')
 	print()
 
 def chooseCave():
